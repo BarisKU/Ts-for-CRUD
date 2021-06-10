1ef13dd758a0cb89d426889de2abcc2329882d5f
BarisKU <85397158+BarisKU@users.noreply.github.com>
2021-06-10 12:26:26 +0300

    Delete forsyaj/src/router directory

--- forsyaj/src/router/router.index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const PersonController = require ('../controller/person.controller');
-
-const router = express.Router();
-router.post('/person.controller',PersonController.createPerson);
-module.exports=router;
