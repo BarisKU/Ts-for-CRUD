70f0d89351867982fd805385f16baabd6d4e1d9b
BarisKU <85397158+BarisKU@users.noreply.github.com>
2021-06-11 15:51:35 +0300

    Update app.service.ts

--- forsyaj/src/service/app.service.ts
+++ forsyaj/src/service/app.service.ts
@@ -1,15 +1,35 @@
 import PersonRepository from '../repository/person.repository'
+import express from "express";
 import {person} from '../interface/Persontype.js'
+import PersonController from '../controller/person.controller'
 /*
 let m : PersonRepository = new PersonService();
 let r = m.createPerson<{ name: string }>({}, "", "","");;
 r.name
 */
-export class PersonService extends PersonRepository {
-                createPerson(personDto:any){
-                const {firstName,lastName,email}=personDto;
-                PersonRepository.createPerson(firstName,lastName,email);
-        }
-
+export class PersonService  {
+                personRepository:PersonRepository;
+                 constructor (){
+                this.personRepository = new PersonRepository;
+                 }
+                async addPerson(persondto:person):Promise<any>{
+                        return new Promise((resolve,reject)=>{
+                                this.addPerson.add(persondto).then((res)=>{
+                                        resolve(this.response.Succes(undefined,res));
+                                })
+                                .catch((err)=>{
+                                        reject(err);
+                                });
+                        });
+               }
 }
+
 export default new PersonService();
+
+
+
+/*
+this.addPerson(person){
+                const {firstName,lastName,email}=personDto;
+                PersonRepository.addPerson(firstName,lastName,email);
+                */
