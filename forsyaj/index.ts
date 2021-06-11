1b8f1d86868dde2c9efc9b0f25ad352b70cbb15a
BarisKU <85397158+BarisKU@users.noreply.github.com>
2021-06-11 15:53:17 +0300

    Update index.ts

--- forsyaj/index.ts
+++ forsyaj/index.ts
@@ -1,5 +1,41 @@
+//const express = require('express');
 import express from 'express';
-const app = express();
-app.use(express.json());
+import PersonController from './controller/person.controller'
+import { KnexDB } from './db/knexfile';
+import ApplicationError from './middleware/appErr'
 
-app.listen(8000,() =>console.log('server 8000 port'));
+class API{
+    app:express.Application;
+    appRouter:express.Router;
+    constructor(){
+        this.app=express();
+        this.appRouter = express.Router();
+        }
+
+    init(){
+        return new Promise((resolve)=>{
+            try{
+            KnexDB.init();
+            this.appConfig();
+            this.routerConfig();
+            }catch(error){
+                console.log(error);
+            }finally{
+                this.app.use(ApplicationError);
+                resolve(true);
+            }
+         })
+    }
+    private appConfig(){
+        this.app.listen(8000,() =>console.log('server port 8000'));
+        this.app.use(express.json());
+    }
+    private routerConfig(){
+        const apiPath="/api";
+        this.app.use(apiPath,this.appRouter);
+        this.appRouter.use("/person.controller",PersonController);
+
+    }
+}
+const app = new API();
+export default app;
