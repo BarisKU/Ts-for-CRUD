8797e2e81d72f39bdaca389363d4e9cac570effb
BarisKU <85397158+BarisKU@users.noreply.github.com>
2021-06-11 15:50:14 +0300

    Update person.controller.ts

--- forsyaj/src/controller/person.controller.ts
+++ forsyaj/src/controller/person.controller.ts
@@ -1,8 +1,27 @@
 
 //const PeersonServicee = require('../service/person')
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
+//import * as express from "express";
 import PersonService from '../service/app.service'
+import schemas from '../validation/app validation'
 export default class PersonController{
+      personService : PersonService;
+      router: express.Router;
+        constructor(){
+        this.router = express.Router();
+        this.personService = new PersonService();
+        this.initializeRoutes();
+        }
+
+    addPerson(req:Request,res:Response,next:NextFunction){
+        const{body}=req;
+        schemas.add.validateAsync(body).then(()=>{
+            this.personService.addPerson(body).then((result=>{
+                return res.status(201).send(result);
+            })) 
+        })
+    }
+    /*
     createPerson(req:Request,res:Response){
             try{
                 const id =  PersonService.createPerson(req.body);
@@ -10,7 +29,37 @@ export default class PersonController{
             }catch(err){
                 console.error(err);
             }
-    }   
+    } 
+    */
+     /*
+    getPersonList(req:Request, res:Response,next:NextFunction){
+        const params = req.query;
+        schemas.list.validateAsync(params)
+        .then(async()=>{
+        let 
+
+        })
+
+    }
+    */
+    deletePerson(req:Request,res:Response,next:NextFunction){
+        try{
+            this.personService.deletePerson(schemas.list.id).then(result)=>{
+                return res.status(200).send(result);
+            }
+        }catch(error){
+            console.log(error);
+        };
+
+
+    }
+    initializeRoutes(){
+    this.router.get("/:id",this.addPerson);
+
+    }
+
+
+
 
 }
 module.exports= new PersonController();
