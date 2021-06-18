cb61b2c944e7234cc5ba05378b899f76f8ad936f
BarisKU <85397158+BarisKU@users.noreply.github.com>
2021-06-18 12:15:40 +0300

    Update app.service.ts

--- forsyaj/src/service/app.service.ts
+++ forsyaj/src/service/app.service.ts
@@ -2,34 +2,36 @@ import PersonRepository from '../repository/person.repository'
 import express from "express";
 import {person} from '../interface/Persontype.js'
 import PersonController from '../controller/person.controller'
+import { ResponsePerson } from '../interface/responsePerson';
 /*
 let m : PersonRepository = new PersonService();
 let r = m.createPerson<{ name: string }>({}, "", "","");;
 r.name
 */
-export class PersonService  {
+export default class PersonService  {
                 personRepository:PersonRepository;
                  constructor (){
                 this.personRepository = new PersonRepository;
                  }
-                async addPerson(persondto:person):Promise<any>{
+               async addPerson(persondto:person):Promise<ResponsePerson>{
                         return new Promise((resolve,reject)=>{
-                                this.addPerson.add(persondto).then((res)=>{
-                                        resolve(this.response.Succes(undefined,res));
+                                this.personRepository.addPerson(persondto).then((res)=>{
+                                        resolve({id:2});
                                 })
                                 .catch((err)=>{
                                         reject(err);
                                 });
                         });
-               }
+               }/*
+               async deletePerson(persondto:person):Promise<ResponsePerson>{
+                return new Promise((resolve,reject)=>{
+                        this.personRepository.deletePerson(persondto).then((res)=>{
+                                resolve({id:2});
+                        })
+                        .catch((err)=>{
+                                reject(err);
+                        });
+                });
+       }
+       */
 }
-
-export default new PersonService();
-
-
-
-/*
-this.addPerson(person){
-                const {firstName,lastName,email}=personDto;
-                PersonRepository.addPerson(firstName,lastName,email);
-                */
